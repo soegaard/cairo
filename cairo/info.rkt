fb2821d915d1a6a817e323507650f4390a0c99f1
Jens Axel Søgaard <jensaxel@soegaard.net>
2021-05-31 23:00:47 +0200

    Updated cairo/info.rkt

--- cairo/info.rkt
+++ cairo/info.rkt
@@ -1,12 +1,6 @@
 #lang info
-
-(define collection 'multi)
-
-(define deps '("cairo-lib"
-               "cairo-test"))
-(define implies '("cairo-lib"
-                  "cairo-test"))
-
-(define pkg-desc "Bindings for the 2d graphics library Cairo mathing the C api.")
-
+(define collection  'multi)
+(define deps        '("cairo-lib" "cairo-test"))
+(define implies     '("cairo-lib" "cairo-test"))
+(define pkg-desc    "Bindings for the 2d graphics library Cairo mathing the C api.")
 (define pkg-authors '(soegaard))
